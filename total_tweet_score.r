b5bc3048b7446fe0d8b32ded277c0438da160377
Bhavani Prasad Rao Ejanthkar <35183461+bhavaniprasad73@users.noreply.github.com>
2018-01-11 22:06:57 -0500

    Tweet Sentiment Score
    Tweet Sentiment Score for Hurricane Harvey

---
+++ total_tweet_score.r
@@ -0,0 +1,34 @@
+    library("plyr")
+    library("stringr")
+
+    setwd("C:/Users/ebhavaniprasad/Desktop/Big_59000")
+    tweet_text= low2
+    vocab_n= negative
+    vocab_p= positive
+    
+    # Here the function will recursively compares the tweet with the word lists and generates score 
+    Polarity <- function(tweet_text, vocab_p, vocab_n, .progress='none')
+    {
+        points1 <- laply(tweet_text, function(tweet_text, vocab_p, vocab_n){
+        tweet1 <- str_split(tweet_text, '\\s+')
+        tweet2 <- unlist(tweet1)
+        p_comp <- match(tweet2, vocab_p)
+        n_comp <- match(tweet2, vocab_n)
+        p_comp <- !is.na(p_comp)
+        n_comp <- !is.na(n_comp)
+        score <- sum(p_comp) - sum(n_comp)
+        pscore = sum(p_comp)
+        nscore = sum(n_comp)
+        
+        return(score)
+      }, vocab_p, vocab_n, .progress=.progress)
+      scores.df <- data.frame(score=points1, text=tweet_text)
+      
+      return(scores.df$score)
+    }
+    
+    # Calling a function by passing parameters 
+    list_score= Polarity(low2, positive, negative)
+    print(list_score)
+    
+    
